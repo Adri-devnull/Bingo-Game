9ee162b9ac8d5e56e22e2cbf83dc6a3d33c2ec4c
Adri-devnull <adri.devnull@gmail.com>
2023-11-22 12:02:07 +0100

    firstchanges

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,31 @@
+// ELEMENTOS DEL DOM
+const boardGameElement = document.getElementById('boardgame');
+const cardUserBingoElement = document.getElementById('user-card-bingo');
+const cardPcBingoElement = document.getElementById('pc-card-bingo');
+
+
+// FUNCION PARA GENERAR NUMEROS ALEATORIOS
+const generateRandomNumbers = () => {
+    return Math.floor(Math.random() * 100);
+}
+
+// FUNCION PARA PINTAR NUMEROS EN LA CARD DEL BINGO
+let numbers = 0;
+const fragment = document.createDocumentFragment();
+const printCardBingo = (card) => {
+    while (numbers < 15) {
+        const cell = document.createElement('span');
+        cell.classList.add('cell');
+        cell.textContent = generateRandomNumbers();
+        fragment.append(cell)
+        numbers++
+    }
+    card.append(fragment);
+    numbers = 0;
+}
+
+printCardBingo(cardUserBingoElement);
+printCardBingo(cardPcBingoElement);
+
+
 
